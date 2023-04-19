21f13f68c52745a6b44847ab383979a53292726e
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 10:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getdimension(){
+	const width=window.innweWidth;
+		const height=window.innerHeight;
+	console.log('Width:',width,'and Height',height);
+	// console.log('Height:${height}');
+}
+window.addEventListner('resize',getdimension);
+getdimension();
