38bcd6b8f690bc140e4beb21b7fd07cdaa6e9ee4
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 12:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//your JS code here. If required.
-function getdimension(){
-	const width=window.innweWidth;
-		const height=window.innerHeight;
-	console.log('Width:',width,'and Height',height);
-	// console.log('Height:${height}');
-}
-window.addEventListener('resize',getdimension);
-getdimension();
+// //your JS code here. If required.
+// function getdimension(){
+// 	const width=window.innweWidth;
+// 		const height=window.innerHeight;
+// 	console.log('Width:',width,'and Height',height);
+// 	// console.log('Height:${height}');
+// }
+// window.addEventListener('resize',getdimension);
+// getdimension();
